7e0235be93bd8667c2b1d73514a89ba0eb749049
anairo98 <161831170+anairo98@users.noreply.github.com>
2024-08-01 15:14:47 +0200

    Update Port

--- notfallapp_main.py
+++ notfallapp_main.py
@@ -35,4 +35,5 @@ def auswertung():
     return render_template('auswertung.html', notfaelle=notfaelle)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
+    
